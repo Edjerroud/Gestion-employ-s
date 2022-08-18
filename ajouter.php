6bd615c932f787557eaec80957eb793a685d0873
EDJERROUD <djerroud13000@gmail.com>
2022-08-18 18:58:32 +0200

    Projet crud

---
+++ ajouter.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="form">
+        <a href="index.php" class="back_btn"><img src="images/flèche.png"> Retour</a>
+        <h2>Ajouter un employé</h2>
+        <p class="erreur_message">
+            Veuillez remplir tous les champs !
+        </p>
+        <form action="" method="POST">
+            <label>Nom</label>
+            <input type="text" name="nom">
+        </form>
+        <form action="" method="POST">
+            <label>Prénom</label>
+            <input type="text" name="prénom">
+        </form>
+        <form action="" method="POST">
+            <label>Age</label>
+            <input type="number" name="age">
+        </form>
+        <form action="" method="POST">
+            <label>Sexe</label>
+            <input type="text" name="Sexe">
+            <input type="submit" value="Ajouter" name="button">
+
+        </form>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Gestion Employés</title>
+</head>
+<body>
+    <div class="container">
+        <a href="ajouter.php" class="Btn_add"><img src="images/plus.png">Ajouter</a>
+
+        <table>
+            <tr id="items">
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Age</th>
+                <th>Sexe</th>
+                <th>Modifier</th>
+                <th>Supprimer</th>
+            </tr>
+            <tr>
+                <td>Elyamine</td>
+                <td>Djerroud</td>
+                <td>38 ans</td>
+                <td>Masculin</td>
+                <td><a href="modifier.php"><img src="images/icons8-crayon-50.png"></a></td>
+                <td><a href="#"><img src="images/icons8-effacer-50.png"></a></td>
+            </tr>
+        </table>
+
+
+
+
+
+
+    </div>
+</body>
+</html>
